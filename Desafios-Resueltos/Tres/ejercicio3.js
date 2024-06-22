085a3b6287b4129b479a04b84225e8139fd38850
Yossec <yosec.arratea22@gmail.com>
2024-06-22 03:12:11 -0500

    ejercicio usando el bucle while

---
+++ Desafios-Resueltos/Tres/ejercicio3.js
@@ -0,0 +1,7 @@
+let valor = prompt("Ingrese un numero para comenzar a contar");
+let contador = 0;
+
+while (contador <= valor ) {
+    console.log(contador );
+    contador += 1;
+}
---
+++ Desafios-Resueltos/Tres/ejercicio4.js
@@ -0,0 +1,6 @@
+let valor = prompt("Ingrese un numero para comenzar a descontar");
+
+while (valor >= 0) {
+    console.log(valor);
+    valor -= 1;
+}
