279141fc68f5242486af84b235cbd4965d27c7ee
Yossec <yosec.arratea22@gmail.com>
2024-06-22 10:56:09 -0500

    mejora para manetenibilidad

--- Logica-uno/script.js
+++ Logica-uno/script.js
@@ -2,9 +2,8 @@
 let numeroSecreto = 7;
 let numeroUsuario = 0;
 let intentos = 1;
-let palabraVeces = 'vez';
-
-
+let palabraVeces = "vez";
+let maximoIntentos = 3;
 
 while (numeroUsuario != numeroSecreto) {
     numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor:");
@@ -15,7 +14,9 @@ while (numeroUsuario != numeroSecreto) {
 
     if (numeroUsuario == numeroSecreto) {
         //Acertamos, fue verdadero la condicion
-        alert(`Acertaste, el número es: ${numeroUsuario}. Lo hiciste en  ${intentos} ${palabraVeces} `);
+        alert(
+            `Acertaste, el número es: ${numeroUsuario}. Lo hiciste en  ${intentos} ${palabraVeces} `
+        );
     } else {
         if (numeroUsuario > numeroSecreto) {
             alert(`El número secreto es menor`);
@@ -23,7 +24,14 @@ while (numeroUsuario != numeroSecreto) {
             alert(`El número secreto es mayor`);
         }
         //Incrementamos el contador cuando no acierta
-        intentos = intentos + 1;
-        palabraVeces = 'veces'
+        //intentos += 1;
+        intentos++;
+
+        palabraVeces = "veces";
+
+        if (intentos > maximoIntentos) {
+            alert(`Llegaste al maximo de ${maximoIntentos} intentos`);
+            break;
+        }
     }
 }
