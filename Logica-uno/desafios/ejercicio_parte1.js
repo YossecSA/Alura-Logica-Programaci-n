a3229d0e3debada7954394c64e054dcd22ed3b32
Yossec <yosec.arratea22@gmail.com>
2024-06-22 01:59:13 -0500

    ejercicios bascio de logica 1

---
+++ Logica-uno/desafios/ejercicio_parte1.js
@@ -0,0 +1,34 @@
+//ejercicio 1
+alert('¡Bienvenida y Bienvenido a nuestro sitio web!');
+
+//ejercicio 2
+let nombre = 'Luna';
+
+//ejercicio 3
+let edad = 25;
+
+//ejercicio 4
+let numeroDeVentas = 50;
+
+//ejercicio 5
+let saldoDisponible = 1000;
+
+//ejercicio 6
+alert('¡Error! Completa todos los campos');
+
+//ejericicio 7
+let mensajeDeError = '¡Error! Completa todos los campos';
+alert(mensajeDeError);
+
+//ejercicio 8
+let nombreUsuario = prompt("Ingrese su nombre de usuario");
+
+//ejercicio 9
+let edadUsuario = prompt("Ingrese su edad");
+
+//ejercicio 10
+if (edadUsuario >= 18) {
+    alert(`${nombreUsuario}, Puedes obtener tu licencia de conducir`);
+} else {
+    alert(`${nombreUsuario}, Aun no puedes obtener tu licencia`);
+}
