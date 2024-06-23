3a9ba5de8627547f91d60794ad50968ee182dea0
Yossec <yosec.arratea22@gmail.com>
2024-06-23 01:48:23 -0500

    ejercicio logica 2

---
+++ Logica-dos/2035-logica-programacion-2-Aula1/app.js
@@ -0,0 +1,12 @@
+//seleccionar elementos del html
+
+let titulo = document.querySelector("h1");
+titulo.textContent = "Juego del Número Secreto";
+
+let parrafo = document.querySelector("p");
+parrafo.textContent = "Indica un número del 1 al 10";
+
+//Funcion para el boton
+function intentoDeUsuario(){
+    
+}
