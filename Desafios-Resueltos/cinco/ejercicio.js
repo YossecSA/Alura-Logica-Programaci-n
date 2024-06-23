8ca22d141903cfb63d4355fd6b571eddd624ffb7
Yossec <yosec.arratea22@gmail.com>
2024-06-23 02:55:22 -0500

    resolucion de los ejecicios

---
+++ Desafios-Resueltos/cinco/ejercicio.js
@@ -0,0 +1,15 @@
+//Ejercicio 4
+function promedio (valor1, valor2, valor3){
+    return (valor1 + valor2 + valor3)/3
+}
+
+//Ejercicio 5
+function mayorNumero(num1, num2){
+    return (num1 > num2 ) ? num1 : num2;
+}
+
+//Ejercicio 6
+
+function multi(num1){
+    return num1 * num1
+}
