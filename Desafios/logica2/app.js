55a4219a6c9ca8bdc5548392450be714abbf74fd
Yossec <yosec.arratea22@gmail.com>
2024-06-23 02:00:34 -0500

    resuleto los ejercicios

--- Desafios/logica2/app.js
+++ Desafios/logica2/app.js
@@ -1,1 +1,26 @@
 
+//Ejercicio 2
+let titulo = document.querySelector('h1');
+titulo.textContent = 'Hora del Desafio';
+
+//Ejercicio 3
+function consola(){
+    console.log('El botón fue clicado');
+}
+
+//Ejercicio 4
+function ciudad(){
+    let nombreCiudad = prompt('¿Nombre de la ciudad de Brasil?')
+    alert(`Estuve en ${nombreCiudad} y me acordé de ti`)
+}
+
+//Ejercicio 5
+function mensaje(){
+    alert('Yo amo JS');
+}
+function suma(){
+    let valor1 = Number(prompt('Ingresa un número: '))
+    let valor2 = Number(prompt('Ingresa un número: '))
+    let suma = valor1 + valor2;
+    alert(`El resultado es ${suma}`)
+}
