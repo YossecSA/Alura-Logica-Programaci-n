0e26c31cd70c08a056d6c6495abd48d386f26122
Yossec <yosec.arratea22@gmail.com>
2024-06-23 02:45:18 -0500

    funciones de buenas practicas

--- Logica-dos/2035-logica-programacion-2-Aula1/app.js
+++ Logica-dos/2035-logica-programacion-2-Aula1/app.js
@@ -1,12 +1,29 @@
-//seleccionar elementos del html
+let numeroSecreto = generarNumeroSecreto();
 
-let titulo = document.querySelector("h1");
-titulo.textContent = "Juego del Número Secreto";
+//Funciones del Sistema
 
-let parrafo = document.querySelector("p");
-parrafo.textContent = "Indica un número del 1 al 10";
-
-//Funcion para el boton
-function intentoDeUsuario(){
-    
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.textContent = texto;
+    return;
 }
+
+function verificarIntento() {
+    let numeroDeUsuario = parseInt(document.getElementById("valorUsuario").value);
+
+    if (numeroDeUsuario === numeroSecreto) {
+        
+    }
+
+    alert(numeroDeUsuario);
+    return;
+}
+
+function generarNumeroSecreto() {
+    let numeroSecreto = Math.floor(Math.random() * 10) + 1;
+    return numeroSecreto;
+}
+
+//Llamada de las funciones
+asignarTextoElemento("h1", "Juego del número secreto!");
+asignarTextoElemento("p", "Indica un número del 1 al 10");
